8e8c7e01375b076e4640da53dc97cd48e0e99f1d
webadib <webadib@gmail.com>
2025-02-06 06:20:19 +0600

    first commit

---
+++ lib/MyHomePage.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+// My home page
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Title',
+        ),
+      ),
+    );
+  }
+}
